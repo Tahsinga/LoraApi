ea0443fcfa1a6d9868d1c5e01123d918341c1439
Tahsinga <user@example.com>
2026-09-05 18:54:57 +0200

    Migrate database during WSGI startup

--- config/config/wsgi.py
+++ config/config/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 
 import os
 
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+django.setup()
+call_command('migrate', interactive=False, verbosity=0)
 application = get_wsgi_application()
